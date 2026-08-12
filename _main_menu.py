8872c74a94b3c472aa8f7b61436bff0e9aaffd11
G Soma Kiran <gvanaja345@gmail.com>
2026-08-13 00:22:02 +0530

    Done Render things , now physics reminas

--- _main_menu.py
+++ _main_menu.py
@@ -1,133 +1,41 @@
+import helpers as h
 
 class MainMenu:
     class __Arrow:
         def __loadAssets(self):
             with open("Arrow.txt" , "r" , encoding="utf-8") as file:
-                self.__texture = file.read()
+                self.visual.addTexture("arrow" , file.read() , dimensions=(7 , 1))
+                self.visual.setPosition((12 , 59))
+                self.visual.setTexture("arrow")
 
-        def __initTrackerVariables(self):
-            self.__visible = True
-            self.__changed = True
-            self.__isPointingStart = True  
+        def __initTrackerVariables(self):  
             self.__lastVisibleTime = 0  
-
-            self.__renderData = [
-                [(59 , 12) , (66 , 13) , "\x1b[38;2;255;255;0m"],
-                [(59 , 17) , (66 , 18) , "\x1b[38;2;255;255;0m"]
+            # self.__renderData = [
+            #     [(59 , 12) , (66 , 13) , "\x1b[38;2;255;255;0m"],
+            #     [(59 , 17) , (66 , 18) , "\x1b[38;2;255;255;0m"]
                 
-            ]
+            # ]
 
         def __init__(self):
+            self.visual = h.Sprite()
             self.__loadAssets()
             self.__initTrackerVariables()
 
         def handleInput(self , * ,input , time):
-            if(input == b"w" and not (self.__isPointingStart)):
-                self.__changed = True
-                self.__visible = True
+            if(input == b"w"):
+                self.visual.setPosition((12 , 59))
                 self.__lastVisibleTime = time
-                self.__isPointingStart = True
-
-            elif( input == b"s" and self.__isPointingStart):
-                self.__changed = True
-                self.__visible = True
+            elif(input == b"s"):
+                self.visual.setPosition((17 , 59))
                 self.__lastVisibleTime = time
-                self.__isPointingStart = False
 
         def update(self , * , time):
-            if( time - self.__lastVisibleTime >= 0.53):
-                self.__visible = not self.__visible
+            if( time - self.__lastVisibleTime >= 0.59):
+                self.visual.toggleVisibility()
                 self.__lastVisibleTime = time
-                self.__changed = True
 
         def render(self):
-            if( self.__visible and self.__changed ):
-                if( self.__isPointingStart ):
-                    # print("hola1")
-                    start = self.__renderData[0][0]
-                    end   = self.__renderData[0][1]
-                    color = self.__renderData[0][2]
-
-                    width  = end[0] - start[0]
-                    height = end[1] - start[1]
-
-                    print(f"\x1b[{start[1]};{start[0]}H" , end = "")
-                    print(f"{color}" , end = "")
-
-                    # for i in range(height + 1):
-                    #     for j in range(width + 1):
-                    #         print(f"\x1b[{start[1] + j};{start[0] + i}H" , end = "")
-                    #         print(self.__texture[width*i + j])
-                    for i, line in enumerate(self.__texture.splitlines()):
-                        print(f"\x1b[{start[1]+i};{start[0]}H{line}", end="")
-                    
-                    print("\x1b[0m" , end="")
-
-                else:
-                    # print("hola2")
-                    start = self.__renderData[1][0]
-                    end   = self.__renderData[1][1]
-                    color = self.__renderData[1][2]
-
-                    width  = end[0] - start[0]
-                    height = end[1] - start[1]
-
-                    print(f"\x1b[{start[1]};{start[0]}H" , end = "")
-                    print(f"{color}" , end = "")
-
-                    # for i in range(height + 1):
-                    #     for j in range(width + 1):
-                    #         print(f"\x1b[{start[1] + j};{start[0] + i}H" , end = "")
-                    #         print(self.__texture[width*i + j])
-                    for i, line in enumerate(self.__texture.splitlines()):
-                        print(f"\x1b[{start[1]+i};{start[0]}H{line}", end="")
-                    
-                    print("\x1b[0m" , end="")
-
-            elif(not self.__visible and self.__changed):
-                if( self.__isPointingStart ):
-                    # print("hola3")
-                    start = self.__renderData[0][0]
-                    end   = self.__renderData[0][1]
-                    color = self.__renderData[0][2]
-
-                    width  = end[0] - start[0]
-                    height = end[1] - start[1]
-
-                    print(f"\x1b[{start[1]};{start[0]}H" , end = "")
-                    print("\x1b[38;2;24;24;24m" , end = "")
-
-                    # for i in range(height + 1):
-                    #     for j in range(width + 1):
-                    #         print(f"\x1b[{start[1] + j};{start[0] + i}H" , end = "")
-                    #         print(" " , end = "")
-                    for i, line in enumerate(self.__texture.splitlines()):
-                        print(f"\x1b[{start[1]+i};{start[0]}H{line}", end="")
-
-                    print("\x1b[0m" , end="")
-
-                else:
-                    # print("hola4")
-                    start = self.__renderData[1][0]
-                    end   = self.__renderData[1][1]
-                    color = self.__renderData[1][2]
-
-                    width  = end[0] - start[0]
-                    height = end[1] - start[1]
-
-                    print(f"\x1b[{start[1]};{start[0]}H" , end = "")
-                    print("\x1b[38;2;24;24;24m" , end = "")
-
-                    # for i in range(height + 1):
-                    #     for j in range(width + 1):
-                    #         print(f"\x1b[{start[1] + j};{start[0] + i}H" , end = "")
-                    #         print(" " , end = "")
-                    for i, line in enumerate(self.__texture.splitlines()):
-                        print(f"\x1b[{start[1]+i};{start[0]}H{line}", end="")
-
-                    print("\x1b[0m" , end="")
-
-            self.__changed = False 
+            self.visual.render() 
 
 
                     
